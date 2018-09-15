9aba5e20024ec9f336fcb11a7c714ea334964244
Steve Laskaridis <stevelaskaridis@gmail.com>
2018-09-15 22:27:25 +0200

    Add timeout of 1sec to splash screen

--- app/src/main/java/com/vdps/ageprivacymobile/SplashActivity.java
+++ app/src/main/java/com/vdps/ageprivacymobile/SplashActivity.java
@@ -6,10 +6,17 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
+    private static long TIMEOUT_MILLIS = 1000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Start home activity
+        try {
+            Thread.sleep(TIMEOUT_MILLIS);
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         startActivity(new Intent(SplashActivity.this, MainActivity.class));
         // close splash activity
         finish();
